cb5644ad05e8d21a70ae8a4296f0eb39fcae3c68
Valentim Araujo <valentim@evodev.com.br>
2021-07-14 20:37:05 -0300

    setup eslint and prettier for all packages

---
+++ packages/backend/src/modules/redirects.ts
@@ -0,0 +1,9 @@
+import { Handler, APIGatewayProxyResult } from 'aws-lambda';
+import middify from '@libs/middify';
+import formatJSONResponse from '@libs/formatJsonResponse';
+
+export const handler: Handler = middify(
+  async (): Promise<APIGatewayProxyResult> => {
+    return formatJSONResponse(404, 'Page Not found');
+  },
+);
